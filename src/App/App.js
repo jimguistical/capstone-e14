78ef67eff4982d107ec28f147aacaea3ab6989a2
Jim Administrator <jamesdavidconner@gamil.com>
2021-06-10 19:29:12 -0500

    auth code added, error on navbar validateDOMnesting

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import './App.scss';
 import Footer from './components/Footer';
 import Jumbo from './components/Jumbo';
 import NavBar from './components/NavBar';
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [sites, setSites] = useState([]);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((userInState) => {
+      if (userInState) {
+        const userInfoObject = {
+          fullName: userInState.displayName,
+          uid: userInState.uid,
+          userName: userInState.email.split('@')[0]
+        };
+        setUser(userInfoObject);
+        // get request here
+        // getSites(userInState.uid).then((sitesArray) => setSites(sitesArray))
+      } else if (user || user === null) {
+        console.warn('not logged in', sites);
+        setUser(false);
+        setSites([]);
+      }
+    });
+  }, []);
   return (
     <div className='App'>
       <Router>
-        <NavBar/>
+        <NavBar user={user}/>
         {/* <Routes/> */}
         <Jumbo/>
         <Footer/>
--- src/App/components/NavBar.js
+++ src/App/components/NavBar.js
@@ -1,51 +1,55 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+
 import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
+  // NavbarBrand,
   Nav,
   NavItem,
-  // Button
+  Button
 } from 'reactstrap';
+import { signInUser, signOutUser } from '../../helpers/auth';
 
-function NavBar() {
+function NavBar({ user }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
-  // const authButtons = () => (
-  //   <>
-  //     <NavItem>
-  //       {
-  //         user !== null
-  //         && <div>
-  //           {
-  //             user
-  //               ? <Button color='danger' onClick={signOutUser}>SIGN OUT</Button>
-  //               : <Button color='info' onClick={signInUser}>SIGN IN</Button>
-  //           }
-  //         </div>
-  //       }
-  //       </NavItem>
-  //   </>
-  // );
+  const authButtons = () => (
+    <>
+      <NavItem>
+        {
+          user !== null
+          && <div>
+            {
+              user
+                ? <Button color='danger' onClick={signOutUser}>SIGN OUT</Button>
+                : <Button color='info' onClick={signInUser}>SIGN IN</Button>
+            }
+          </div>
+        }
+        </NavItem>
+    </>
+  );
   return (
     <div>
       <div>
       <Navbar dark color="dark" expand="md">
-        <NavbarBrand href="/">Social Services Explorer</NavbarBrand>
+        <Link className="nav-brand" to="/">Social Services Explorer</Link>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-            <Link className="nav-link" to="/add-player">Browse Service Sites</Link>
-
+            <Link className="nav-link" to="/browse">Browse Service Sites</Link>
             </NavItem>
             <NavItem>
-            <Link className="nav-link" to="/add-player">My List</Link>
+            <Link className="nav-link" to="/mylist">My List</Link>
+            </NavItem>
+            <NavItem>
+              {authButtons()}
             </NavItem>
           </Nav>
         </Collapse>
@@ -56,7 +60,7 @@ function NavBar() {
 }
 
 NavBar.propTypes = {
-
+  user: PropTypes.any
 };
 
 export default NavBar;

013f98db135248529f0a69532a5f11f944aa7887
Jim Administrator <jamesdavidconner@gamil.com>
2021-06-13 15:22:33 -0500

    cleaned up files

--- src/App/views/ListView.js
+++ src/App/views/ListView.js
@@ -1,35 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'reactstrap';
+import { Button, Container } from 'reactstrap';
 import ListCard from '../components/ListCard';
 import { createList, getList } from '../../helpers/data/listData';
 
 function ListView({ user }) {
+  // const [listButton, setListButton] = useState(true);
   const [listArray, setListArray] = useState([
+
   ]);
+
+  useEffect(() => {
+    getList(user.uid).then((listArrayResponse) => (setListArray(listArrayResponse)));
+  }, []);
 
   const handleClick = () => {
     createList(listArray, user.uid).then((getList(user.uid)));
   };
 
-  // useEffect(() => {
-  //   getList(user.uid).then((listArrayResponse) => (setListArray(listArrayResponse)));
-  // }, []);
-
   return (
-    <div className='cardsHolder'>
+    <>
+    <Container>
       <Button color='warning'
               onClick={() => handleClick()}>Create Your List
       </Button>
+    <div className='cardsHolder'>
       {listArray.map((listObj) => (
         <ListCard
         user={user}
-        key={listObj.firebaseKey}
+        key={listObj.business}
         setListArray={setListArray}
         {...listObj}
         />
       ))}
     </div>
+        </Container>
+    </>
   );
 }
 
@@ -37,7 +43,6 @@ ListView.propTypes = {
   user: PropTypes.any,
   listArray: PropTypes.array,
   setListArray: PropTypes.func
-
 };
 
 export default ListView;

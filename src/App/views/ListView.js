fe1e201f6a7c0a97f9c832628a5d8620df406749
Jim Administrator <jamesdavidconner@gamil.com>
2021-06-20 08:52:45 -0500

    added SiteCard component to ListView

--- src/App/views/ListView.js
+++ src/App/views/ListView.js
@@ -8,24 +8,16 @@ import PropTypes from 'prop-types';
 import ListForm from '../components/ListForm';
 import ListCard from '../components/ListCard';
 import { getListByListName } from '../../helpers/data/listData';
+import SiteCard from '../components/SiteCard';
 
 function ListView({ user }) {
-  // const [listObj, setListObj] = useState({});
   const [listNameArray, setListNameArray] = useState([]);
-  // const [siteArray, setSiteArray] = useState([]);
+  const [siteArray, setSiteArray] = useState([]);
 
   useEffect(() => {
     getListByListName(user.uid).then((response) => (setListNameArray(response)));
-    // debugger;
-    // setListObj(listObj);
-    // console.warn(listObj);
-  }, []);
-
-  // useEffect(() => {
-  //   getList(user.uid).then((response) => (setListObj(response)));
-  //   console.warn(listObj);
-  // }, []);
   // need to use Promise.All to get sites with user & ListID
+  }, []);
 
   return (
     <>
@@ -38,7 +30,6 @@ function ListView({ user }) {
 <div className='cardsHolder'>
         {listNameArray.map((listNameInfo) => (
           <ListCard
-          // listName as key only works if I have on List Obj
           user={user}
           key={listNameInfo.listID}
           setListNameArray={setListNameArray}
@@ -46,19 +37,15 @@ function ListView({ user }) {
           />
         ))}
       </div>
-      {/* <div className='cardsHolder'>
+      <div className='cardsHolder'>
         {siteArray.map((siteInfo) => (
-          console.warn(setSiteArray(siteInfo))
+          <SiteCard
+          user={user}
+          key={siteInfo.building}
+          />
           // pass listInfo.listID instead of just having it as key
-          // SiteCard here
         ))}
-      </div> */}
-          {/* {<h3>
-            { listObj.Name === null
-              ? 'Please Create a List or Browse Service Sites'
-              : ''
-            }
-          </h3>} */}
+      </div>
     </div>
     </>
   );
